ac40defb770bd7ddc7d8b49109ae6d369ed96fdf
Misha Nikanorov <fobozsb@gmail.com>
2024-03-12 15:52:15 -0700

    [feature/sendable-test]: Added `xcprivacy`

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,6 +24,12 @@ let package = Package(
       dependencies: [
         .product(name: "Logging", package: "swift-log"),
         .product(name: "LoggingSyslog", package: "swift-log-syslog", condition: .when(platforms: [.android, .linux, .wasi]))
+      ],
+      resources: [
+        .copy("Privacy/PrivacyInfo.xcprivacy")
+      ],
+      swiftSettings: [
+        .enableExperimentalFeature("StrictConcurrency=complete")
       ]
     ),
     .testTarget(
